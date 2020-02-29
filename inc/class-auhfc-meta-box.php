9a23d0cdbae84ce6a403d07df30e9220bb09d631
Aleksandar Urosevic <urke.kg@gmail.com>
2020-02-29 13:46:18 +0100

    Fix stripping backslashes on post/page update

--- inc/class-auhfc-meta-box.php
+++ inc/class-auhfc-meta-box.php
@@ -53,7 +53,7 @@ abstract class AUHfc_Meta_Box {
 			$auhfc['behavior'] = ( ! empty( $_POST['auhfc']['behavior'] ) ) ? $_POST['auhfc']['behavior'] : '';
 
 			if ( ! empty( $auhfc ) ) {
-				update_post_meta( $post_id, '_auhfc', $auhfc );
+				update_post_meta( $post_id, '_auhfc', wp_slash( $auhfc ) );
 			}
 		}
 
@@ -66,7 +66,7 @@ abstract class AUHfc_Meta_Box {
 	 */
 	public static function html( $post ) {
 		wp_nonce_field( '_head_footer_code_nonce', 'head_footer_code_nonce' ); ?>
-		<p>Here you can insert article specific code for Head (before the <code>&lt;/head&gt;</code>) and Footer (before the <code>&lt;/body&gt;</code>) sections. They work in exactly the same way as site-wide code, which you can configure under <a href="tools.php?page=head_footer_code">Tools / Head &amp; Footer Code</a>.</p>
+		<p>Here you can insert article specific code for Head (before the <code>&lt;/head&gt;</code>), Body (after the <code>&lt;body&gt;</code>) and Footer (before the <code>&lt;/body&gt;</code>) sections. They work in exactly the same way as site-wide code, which you can configure under <a href="tools.php?page=head_footer_code">Tools / Head &amp; Footer Code</a>.</p>
 		<label><?php esc_attr_e( 'Behavior', 'head-footer-code' ); ?></label><br />
 		<select name="auhfc[behavior]" id="auhfc_behavior_replace">
 			<option value="append" <?php echo ( 'append' === auhfc_get_meta( 'behavior' ) ) ? 'selected' : ''; ?>>Append to the site-wide code</option>
